957833568bcc28082e68af52c56a3e61a76955aa
Lawrence Weinstein <larryweinstein42@gmail.com>
2023-12-15 17:43:50 -0700

    Answered arithmetic operator solution.

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+        float a = 1.0f + 2.0f;
+        System.out.println("1.0 + 2.0 = " + a);
+        float b = 5.0f - 4.0f;
+        System.out.println("5.0 - 4.0 = " + b);
+        float c = 4.0f * 2.0f;
+        System.out.println("4.0 * 2.0 = " + c);
+        float d = 13.0f / 5.0f;
+        System.out.println("13.0 / 5.0 = " + d);
+        float e = 13.0f % 5.0f;
+        System.out.println("13.0 % 5.0 = " + e);
+        int f = 1 + 2;
+        System.out.println("1 + 2 = " + f);
+        int g = 5 - 4;
+        System.out.println("5 - 4 =" + g);
+        int h = 4 * 2;
+        System.out.println("4 * 2 = " + h);
+        int i = 13 / 5;
+        System.out.println("13 / 5 = " + i);
+        int j = 13 % 5;
+        System.out.println("13 % 5 = " + j);
+    }
+}
